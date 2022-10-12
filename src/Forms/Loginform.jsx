3ebeef81d9ea124c874bc21c6f041ce566fa414f
unknown <tapiajnel606@gmail.com>
2022-10-12 23:05:53 +0800

    added log in form

--- src/Forms/Loginform.jsx
+++ src/Forms/Loginform.jsx
@@ -1,21 +1,21 @@
 const Loginform = () => {
     return (
-        <div className="container">
+        <div className="container col-md-4 mt-5 py-5">
             <form>
-                <div class="mb-3">
+                <div class="mb-3  mx-auto">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div class="mb-3">
+                <div class="mb-3  mx-auto">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" />
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="mb-3  mx-auto">
+                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" />
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary mx-auto">Submit</button>
             </form>
         </div>
     );
