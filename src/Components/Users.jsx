963288d83a1d20b4955bfbbdbd05061c11b98507
KingNelx <tapiajonel606@gmail.com>
2022-11-10 19:42:05 +0800

    added test

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -8,7 +8,9 @@ const Users = () => {
                     <div class="col-6">
                         <div class="p-3 border bg-light">
                             <div class="card" >
-                                <img src="https://virac-catanduanes.gov.ph/wp-content/uploads/2021/06/pho.png" class="card-img-top" alt="..." />
+                                <img src="https://virac-catanduanes.gov.ph/wp-content/uploads/2021/06/pho.png" class="card-img-top" alt="..." style={
+                                    { height: "40vh", width: "50%" }
+                                } />
                                 <div class="card-body">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
