fbf96f9e0fe122081e3fb660d5fdb27cc14fb36c
Jonel Tapia <tapiajonel606@gmail.com>
2022-11-20 05:42:07 +0800

    added logo for users

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 const Users = () => {
-    const pho = "https://scontent.fmnl25-4.fna.fbcdn.net/v/t39.30808-6/291014256_615903669968840_6190598407861528504_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGCix8EvGHScz336HNfp5H_35VMbdJlczbflUxt0mVzNpRa3oMy5c96zv5YwfdBYXPktVHWcJspunZQPOnLuKAT&_nc_ohc=Dex9nJVKVsUAX9ypftw&_nc_ht=scontent.fmnl25-4.fna&oh=00_AfB2dlJWd1CR4rW8JF_iYYf8O577LIC3OXlVK_mIgRWjjw&oe=6377A17D"
+    const pho = "https://virac-catanduanes.gov.ph/wp-content/uploads/2021/06/pho.png"
     const patient = "https://d1nhio0ox7pgb.cloudfront.net/_img/v_collection_png/512x512/shadow/patient.png"
     const rhu = "https://cdn-icons-png.flaticon.com/512/504/504203.png"
 
